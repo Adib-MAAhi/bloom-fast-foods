0c2fc9b1be4e4ee636dccc599cfe3ff10fa9350e
MAAhi-Bloom <czone3144@gmail.com>
2024-11-19 13:14:20 +0600

    add new images

--- fast-fd/src/ui/Header.jsx
+++ fast-fd/src/ui/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
         <header className="flex flex-col shadow-md z-10 top-0 left-0 fixed w-full justify-between items-center p-2 sm:flex-row bg-yellow-500 text-stone-800 ">
             <div className="flex items-center space-x-4">
                 <Link className="uppercase text-orange-800 font-semibold text-xl " to="/">
-                    Bloom-Burgers
+                    <img className="w-6 h-8" src="images/BloomBurger.png" alt="" />
                 </Link>
             </div>
             <div>
