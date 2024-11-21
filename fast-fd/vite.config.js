6a5c0b230376630ce2aa27b8ee93aec18e7f3265
MAAhi-Bloom <czone3144@gmail.com>
2024-11-21 09:04:25 +0600

    update vite.config and package.json file

--- fast-fd/vite.config.js
+++ fast-fd/vite.config.js
@@ -1,12 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
+/*export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',  // You can keep this as it is
+  },
+  server: {
+    port: process.env.PORT || 3000,  // Use the PORT provided by Render or fallback to 3000
+    host: '0.0.0.0',  // Bind the server to all network interfaces
+  },
+});*/
+
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',  
+    outDir: 'dist',
   },
-})
-
+  server: {
+    port: 3000,  
+    host: '0.0.0.0',  
+  }
+});
 
